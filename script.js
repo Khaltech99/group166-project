fa8d1a31c98c929b6ca9538f258e1cb3845c8f97
lawal azeez <olatunjibabatundejunior@gmail.com>
2022-05-15 19:08:57 -0700

    ulterations

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const hidden = document.querySelector(".hidden");
 const menu = document.querySelector(".icon--menu");
 const logo = document.querySelector(".logo");
 const navbar = document.querySelector(".navbar");
+const listItem = document.querySelector(".list__item");
 
 logo.addEventListener("click", function() {
     hidden.classList.add("hidden");
@@ -16,6 +17,7 @@ document.addEventListener("keydown", function(event) {
         if (!navList.classList.contains("hidden")) {
             hidden.classList.add("hidden");
             menu.style.opacity = ".7";
+            listItem.style.transition = ".4s";
         }
     }
 });
@@ -23,7 +25,9 @@ menu.addEventListener("click", function() {
     hidden.classList.toggle("hidden");
     if (navList.classList.contains("hidden")) {
         menu.style.opacity = ".7";
+        listItem.transition = "margin .4s";
     } else if (!navList.classList.contains("hidden")) {
         menu.style.opacity = "1";
+        listItem.style.transition = "margin .4s";
     }
 });
